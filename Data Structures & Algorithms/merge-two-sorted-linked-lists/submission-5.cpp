9a8a5f40a49a9fd8dc6632d91e74b18a55b1ca4c
James Au <161009161+acp02@users.noreply.github.com>
2026-09-13 17:24:58 +0200

    Add: merge-two-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-5.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == nullptr) return list2;
+        if(list2 == nullptr) return list1;
+        
+        ListNode* head;
+        if(list1->val <= list2->val) {
+            head = list1;
+            list1 = list1->next;
+        }
+        else {
+            head = list2;
+            list2 = list2->next;
+        }
+        ListNode* prevNode = head;
+
+        while(list1 != nullptr && list2 != nullptr) {
+            if(list1->val <= list2->val) {
+                prevNode->next = list1;
+                list1 = list1->next;
+                prevNode = prevNode->next;
+            }   else {
+                prevNode->next = list2;
+                list2 = list2->next;
+                prevNode = prevNode->next;
+            }
+        }
+
+        if(list1 == nullptr) prevNode->next = list2;
+        else prevNode->next = list1;
+        return head;
+    }
+};
