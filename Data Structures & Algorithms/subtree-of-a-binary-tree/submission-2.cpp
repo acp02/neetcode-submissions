52bd0753e09d7e62b15c4f386c922920274c4bc2
James Au <161009161+acp02@users.noreply.github.com>
2026-09-14 22:21:51 +0200

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        // Base Cases
+        if(subRoot == nullptr) return false;
+        if(!root && !subRoot) return true;
+        if((!root && subRoot) || (root && !subRoot)) return false;
+
+        // Recursive Steps
+        bool state = false;
+        if(root->val != subRoot->val) { // values do not match, keep tracing down
+            bool leftState = isSubtree(root->left, subRoot);
+            bool rightState = isSubtree(root->right, subRoot);
+            state = leftState || rightState; // so it can return true if there's subtree below the original tree
+        } else if (root->val == subRoot->val) {
+            state = sameTree(root, subRoot);
+            if(state) return true; // values and structures match
+            else { // the two values are the same, but structures of two trees do not match each other
+                bool leftState = isSubtree(root->left, subRoot);
+                bool rightState = isSubtree(root->right, subRoot);
+                state = leftState || rightState;
+            } 
+        }
+        return state; // so it can return true if there's subtree below the original tree
+    }
+
+    bool sameTree(TreeNode* root1, TreeNode* root2) { // basically last question
+        if(!root1 && !root2) return true;
+        if((!root1 && root2) || (root1 && !root2)) return false;
+
+        bool leftState = sameTree(root1->left, root2->left);
+        bool rightState = sameTree(root1->right, root2->right);
+        bool valState = true;
+        if(root1->val != root2->val) valState = false;
+        return leftState && rightState && valState;
+    }
+};
